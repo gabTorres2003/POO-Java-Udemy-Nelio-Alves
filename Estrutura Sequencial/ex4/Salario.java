2fe85abb7e9d9aef7a2de6aa8449368874882ba6
Gabriel Torres <gabrielstorres08@gmail.com>
2025-10-18 10:58:26 -0300

    Ex4 Estrutura Sequencial

---
+++ Estrutura Sequencial/ex4/Salario.java
@@ -0,0 +1,22 @@
+package ex4;
+
+import java.util.Scanner;
+
+public class Salario {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int horasTrabalhadas;
+        double valorHora;
+
+        System.out.println("Total de horas trabalhadas: ");
+        horasTrabalhadas = sc.nextInt();
+
+        System.out.println("Valor da hora: ");
+        valorHora = sc.nextDouble();
+
+        double salario = horasTrabalhadas * valorHora;
+        System.out.println("Salário: " + salario);
+
+        sc.close();
+    }
+}
