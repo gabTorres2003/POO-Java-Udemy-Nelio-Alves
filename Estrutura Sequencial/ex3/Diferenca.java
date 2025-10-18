5a8bbaedde5dc4571ff7d496aa0c6ce144edea8a
Gabriel Torres <gabrielstorres08@gmail.com>
2025-10-18 10:54:10 -0300

    Ex3 Estrutura Sequencial

---
+++ Estrutura Sequencial/ex3/Diferenca.java
@@ -0,0 +1,25 @@
+package ex3;
+
+import java.util.Scanner;
+
+public class Diferenca {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, C, D;
+
+        System.out.println("Valor de A: ");
+        A = sc.nextInt();
+        System.out.println("Valor de B: ");
+        B = sc.nextInt();
+        System.out.println("Valor de C: ");
+        C = sc.nextInt();
+        System.out.println("Valor de D: ");
+        D = sc.nextInt();
+
+        int diferenca = (A * B) - (C * D);
+
+        System.out.println("Total: " + diferenca);
+
+        sc.close();
+    }
+}
