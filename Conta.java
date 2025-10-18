3914bf1ef08c64cb8c0cbe9fc155897af0f7cde4
Gabriel Torres <gabrielstorres08@gmail.com>
2025-10-18 11:37:32 -0300

    Add Construtores Ex e Update folders name

---
+++ Conta.java
@@ -0,0 +1,46 @@
+public class Conta {
+    
+    private int numeroDaConta;
+    private String nomeTitular;
+    private double saldo;
+
+    public Conta(int numeroDaConta, String nomeTitular, double saldoInicial){
+        this.nomeTitular = nomeTitular;
+        this.numeroDaConta = numeroDaConta;
+        depositar(saldoInicial);
+    }
+
+    public Conta(int numeroDaConta, String nomeTitular){
+        this.nomeTitular = nomeTitular;
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar (double valor){
+        saldo -= (valor + 5);
+    }
+
+    public String toString(){
+        return "Conta: " + numeroDaConta + ", titular: " + nomeTitular + ", saldo: " + saldo;
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroDaConta;
+        String depositoInicial;
+        String nomeDoTitular;
+        double valorDepInicial;
+        Conta conta;
+
+        
+        System.out.print("Numero da conta: ");
+        numeroDaConta = scanner.nextInt();
+        System.out.print("Nome do titular: ");
+        scanner.nextLine();
+        nomeDoTitular = scanner.nextLine();
+        System.out.printf("Depósito inicial? (Y/N): ");
+        depositoInicial = scanner.nextLine();
+
+        
+        if (depositoInicial.equalsIgnoreCase("Y")) {
+            System.out.printf("Valor do depósito inicial: ");
+            valorDepInicial = scanner.nextDouble();
+            conta = new Conta(numeroDaConta, nomeDoTitular, valorDepInicial);
+        }else{
+            conta = new Conta(numeroDaConta, nomeDoTitular);
+        }
+
+        System.out.println("Dados da conta: " + conta.toString());
+        
+        System.out.println("Valor de depósito: ");
+        double valorDeposito = scanner.nextDouble();
+        conta.depositar(valorDeposito);
+        System.out.println("Dados atualizados da conta: " + conta.toString());
+        
+        System.out.println("Valor de saque: ");
+        double valorSaque = scanner.nextDouble();
+        conta.depositar(valorSaque);
+        System.out.println("Dados atualizados da conta: " + conta.toString());
+
+        scanner.close();
+    }
+}
