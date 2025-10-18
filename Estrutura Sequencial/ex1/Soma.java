5e28cf3f12764210c6a43f307b3c9f944efad39c
Gabriel Torres <gabrielstorres08@gmail.com>
2025-10-18 10:53:31 -0300

    Ex2 Estrutura Sequencial

---
+++ Estrutura Sequencial/ex1/Soma.java
@@ -0,0 +1,21 @@
+package ex1;
+
+import java.util.Scanner;
+
+class Soma {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int valor1, valor2, soma;
+        
+        System.out.println();
+        valor1 = teclado.nextInt();
+        valor2 = teclado.nextInt();
+
+        soma = valor1 + valor2;
+
+        System.out.println("Soma: " + soma);
+
+
+        teclado.close();   
+    }
+}
