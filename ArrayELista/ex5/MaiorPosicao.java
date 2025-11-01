107ef03c579042f22ae37b2d98a53fea8783331b
Gabriel Torres <gabrielstorres08@gmail.com>
2025-11-01 13:49:46 -0300

    Add ex5

--- ArrayELista/ex5/MaiorPosicao.java
+++ ArrayELista/ex5/MaiorPosicao.java
@@ -1,7 +1,29 @@
 package ArrayELista.ex5;
 
+import java.util.Scanner;
+
 public class MaiorPosicao {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números? ");
+        int totalNumeros = sc.nextInt();
+
+        int[] numerosReais = new int[totalNumeros];
+        int maiorNumero=0;
+        int posicaoMaiorNumero=0;
+
+        for (int i=0; i < totalNumeros; i++){
+            numerosReais[i] = sc.nextInt();
+            if (numerosReais[i] > maiorNumero) {
+                maiorNumero = numerosReais[i];
+                posicaoMaiorNumero = i;
+            }
+        }
+
+        System.out.println("Maior número: " + maiorNumero);
+        System.out.println("Posição do maior número: " + posicaoMaiorNumero);
+
+        sc.close();
     }
 }
